910aad0a4940bd257c84500759ab7466d9ae0340
HayderBarrak <hayder.barrak@esprit.tn>
2019-01-08 14:19:36 +0100

    haroun commit

--- src/main/java/guru/springframework/haroun/test.java
+++
@@ -1,7 +0,0 @@
-package guru.springframework.haroun;
-
-public class test {
-    public test(){
-        System.out.println("hello haroun");
-    }
-}
---
+++ src/main/java/guru/springframework/test2/test.java
@@ -0,0 +1,7 @@
+package guru.springframework.test2;
+
+public class test {
+    public test(){
+        System.out.println("hello");
+    }
+}
