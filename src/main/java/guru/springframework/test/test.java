b3be95575b952afb6e7601c68223d03368abd112
HayderBarrak <hayder.barrak@esprit.tn>
2019-01-08 10:51:07 +0100

    second commit

--- src/main/java/guru/springframework/test/test.java
+++ src/main/java/guru/springframework/test/test.java
@@ -4,5 +4,6 @@ public class test {
     public test (){
         System.out.println("it works");
         System.out.println("it works correctly");
+        System.out.println("test jenkins");
     }
 }
